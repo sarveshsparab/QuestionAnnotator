25eb3c0688d3ec85a3a3a2a5ce2dcddff5f79312
sarvesh <sarveshsparab@gmail.com>
2016-04-16 16:34:27 +0530

    Tokenizer commented

--- src/semantics/Tokenizer.java
+++ src/semantics/Tokenizer.java
@@ -5,18 +5,34 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import commandcentral.Config;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.process.CoreLabelTokenFactory;
 import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.process.TokenizerFactory;
 
+/**
+ * @category Class
+ * @purpose For stripping the sentence and creating tokens
+ * @author Sarvesh
+ */
 public class Tokenizer {
 	public TokenizerFactory<CoreLabel> tf; 
 	private int verbose;
-	private static Helper helper = new Helper();
+	/**
+     * @category Constructor 
+     * @argument verbose : A integer flag for verbose printing
+     * @author Sarvesh
+     */
 	public Tokenizer(int verbose){
 		this.verbose = verbose;
 	}
+	/**
+     * @category Function(Private)
+     * @argument sentence : Sentence String
+     * @return A list of tokenWords
+     * @author Sarvesh
+     */
 	private List<CoreLabel> makeTokens (String sentence){
 		if(tf == null){
 			tf = PTBTokenizer.factory(new CoreLabelTokenFactory(),"untokenizable=noneDelete");
@@ -24,8 +40,14 @@ public class Tokenizer {
 		List<CoreLabel> tokenWords = tf.getTokenizer(new StringReader(sentence)).tokenize();
 		return tokenWords;
 	}
+	/**
+     * @category Function
+     * @argument sentence : Sentence String
+     * @return A list of tokens
+     * @author Sarvesh
+     */
 	public List<String> tokenize (String sentence){
-		helper.printVerbose(verbose, "Tokenizing Starts....");
+		Config.helper.printVerbose(verbose, "Tokenizing Starts....");
 		List<String> tokens = new LinkedList<String>();
 		List<CoreLabel> tokenWords = makeTokens(sentence);
 		Iterator<CoreLabel> itr = tokenWords.iterator(); 	
@@ -33,7 +55,7 @@ public class Tokenizer {
 	         CoreLabel element = itr.next();
 	         tokens.add(element.word());
 	    }
-		helper.printVerbose(verbose, "Tokenizing Starts....");
+		Config.helper.printVerbose(verbose, "Tokenizing Starts....");
 		return tokens;
 	}
 }
