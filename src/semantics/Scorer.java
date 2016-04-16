28f67d799a593d36406d4695bb45aed98b9558da
sarvesh <sarveshsparab@gmail.com>
2016-04-16 13:03:34 +0530

    Scorer commented

--- src/semantics/Scorer.java
+++ src/semantics/Scorer.java
@@ -1,15 +1,25 @@
 package semantics;
 
 import java.util.List;
-
+import commandcentral.Config;
+/**
+ * @category Class
+ * @purpose To get score between two vectors
+ * @author Sarvesh
+ */
 public class Scorer {
-	private static Helper helper = new Helper();
-	
+	/**
+     * @category Function
+     * @argument vect1 : Vectorized form of first sentence
+     * @argument vect2 : Vectorized form of Second sentence
+     * @return A Double score value based on cosine similarity
+     * @author Sarvesh
+     */
 	public Double cosSim(List<Double> vect1,List<Double> vect2,int len){
 		Double sim=0.0;
-		Double len1 = helper.vectLength(vect1), len2 = helper.vectLength(vect2);
+		Double len1 = Config.helper.vectLength(vect1), len2 = Config.helper.vectLength(vect2);
 		if(len1!=0 && len2!=0)
-			sim = helper.vectDotProduct(vect1, vect2, len)/(len1*len2);
+			sim = Config.helper.vectDotProduct(vect1, vect2, len)/(len1*len2);
 		return sim;
 	}
 	
