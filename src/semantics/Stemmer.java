00b1aa0ede95b675889e5c05c43ffcc0e8006577
sarvesh <sarveshsparab@gmail.com>
2016-04-16 13:20:15 +0530

    Stemmer commented

--- src/semantics/Stemmer.java
+++ src/semantics/Stemmer.java
@@ -4,19 +4,41 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import org.tartarus.snowball.ext.PorterStemmer;
-
+/**
+ * @category Class
+ * @purpose For stemming the words
+ * @author Sarvesh
+ */
 public class Stemmer {
 	private int verbose;
 	private static Helper helper = new Helper();
+	/**
+     * @category Constructor 
+     * @argument verbose : A integer flag for verbose printing
+     * @author Sarvesh
+     */
 	public Stemmer(int verbose){
 		this.verbose = verbose;
 	}
+	/**
+     * @category Function(Private)
+     * @argument word : String to be stemmed
+     * @return Returns the stemmed form of the word
+     * @author Sarvesh
+     */
 	private String SnowBallStemmer(String word) { //Snowball stemmer (Porter 2)
 	    PorterStemmer state = new PorterStemmer(); 
 	    state.setCurrent(word); 
 	    state.stem(); 
 	    return state.getCurrent(); 
 	}
+	/**
+     * @category Function
+     * @argument words : Linked list of words to be stemmed
+     * @argument trie : The dictionary of english words used to verify the integrity of the stemmed word
+     * @return A linked list of the corresponding stemmed words
+     * @author Sarvesh
+     */
 	public List<String> stemmatize(List<String> words, Trie trie){
 		helper.printVerbose(verbose, "Stemming Starts....");
 		List<String> stems = new LinkedList<String>();
